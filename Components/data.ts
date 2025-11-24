34b14a2179ec73662591cf071add26b6878f29ad
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-11-24 14:31:31 -0300

    feat: dummyData to save all profileData

---
+++ Components/data.ts
@@ -0,0 +1,14 @@
+export const Data = [
+     {
+        "id": 0,
+        "img": "/ana.png",
+        "name": "Ana Silva",
+        "cargo": "Desenvolvedora Full Stack",
+        "social": [
+            {"title": "Seguidores", "numbers": 980},
+            {"title": "Seguindo", "numbers": 180},
+            {"title": "Projectos", "numbers":  42}
+        ],
+        "bio": "Apaixonada por criar experiências digitais incríveis. Especialista em React e Node.js."
+    }
+]
